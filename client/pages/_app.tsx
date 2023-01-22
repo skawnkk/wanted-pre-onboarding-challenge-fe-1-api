28100f9967666a7bcc2361368045f2bf5a0d1e42
김남주 <namju.kim@tictoccroc.com>
2023-01-22 19:31:01 +0900

    [feat] errorBoundary

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,12 +5,14 @@ import {
   QueryCache,
   QueryClient,
   QueryClientProvider,
+  QueryErrorResetBoundary, useQueryErrorResetBoundary,
 } from '@tanstack/react-query'
 import {ReactElement, ReactNode} from "react";
 import {NextPage} from "next";
 import {isAxiosError} from "axios";
 import {createStandaloneToast} from '@chakra-ui/react'
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import GlobalErrorBoundary from "../src/components/error/GlobalErrorBoundary";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -30,26 +32,45 @@ function queryErrorHandler(error: string): void {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const queryClient = new QueryClient({
-    queryCache: new QueryCache({
-      onError: (error) => {
-        if (isAxiosError(error)) {
-          queryErrorHandler(error.response?.data.details || error.message)
-        }
+    defaultOptions: {
+      queries: {
+        useErrorBoundary: true,
+        suspense: true,
       }
-    })
-  });
+    },
+  })
+
+  // onError: (error) => {
+  //   if (isAxiosError(error)) {
+  //     queryErrorHandler(error.response?.data.details || error.message)
+  //   }
+  // }
 
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        {getLayout(
-          <>
-            <Component {...pageProps} />
-            <ToastContainer/>
-          </>
-        )}
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <GlobalErrorBoundary
+              //onReset={reset}
+              // fallbackRender={({ resetErrorBoundary }) => (
+              //   <div>
+              //     There was an error!
+              //     <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+              //   </div>
+              // )}
+            >
+              {getLayout(
+                <>
+                  <Component {...pageProps} />
+                  {/*<ToastContainer/>*/}
+                </>
+              )}
+            </GlobalErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
         <ReactQueryDevtools initialIsOpen />
       </QueryClientProvider>
     </RecoilRoot>
